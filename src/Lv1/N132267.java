519333c1d22b19e3799e588adf8f89f5be0e2693
jinhwan Bea <123534245+JinhwanB@users.noreply.github.com>
2024-03-04 12:42:32 +0900

    Update N132267.java

--- src/Lv1/N132267.java
+++ src/Lv1/N132267.java
@@ -1,9 +1,10 @@
 package Lv1;
 
 // 콜라 문제
+// a 개를 가져다주면 b 개를 돌려 주는 가게가 있을 때 처음 가지고 있는 수를 n 이라고 한다면 총 몇 개를 돌려받을 수 있는지를 리턴
 public class N132267 {
     public static int solution(int a, int b, int n){
-        if(n < a) return 0;
+        if(n < a) return 0; // n 은 a 보다 커야함
 
         int result = 0;
         while(n >= a){
@@ -12,7 +13,7 @@ public class N132267 {
             if(n % a == 0){
                 n = n / a * b;
             }else{
-                n = (n / a * b) + n % a;
+                n = (n / a * b) + n % a; // 총 3 개를 가지고 있고 2 개를 주면 1 개를 돌려받는다 했을 때 3 개 중 2 개를 반납하고 1개를 받는다. 따라서 원래 있던 1 개를 더해줘야 한다.
             }
 
             result += get;
