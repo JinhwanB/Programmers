475795c586be2180d641255d0b7428306a47c1fe
jinhwan Bea <123534245+JinhwanB@users.noreply.github.com>
2024-03-04 12:37:40 +0900

    Update IDontLikeEnglish.java

--- src/Lv0/IDontLikeEnglish.java
+++ src/Lv0/IDontLikeEnglish.java
@@ -3,6 +3,8 @@ package Lv0;
 import java.util.HashMap;
 import java.util.Map;
 
+// 영어가 싫어요
+// 수를 영단어로 바꿔 조합한 문자열이 주어질 때 다시 수로 바꾸는 문제
 public class IDontLikeEnglish {
     public static long solution(String numbers){
         Map<String, Integer> map = new HashMap<>();
