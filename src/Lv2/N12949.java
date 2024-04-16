102325243feb931751405804b4b7cfd4534fa340
jinhwan Bea <paewhal20@gmail.com>
2024-04-17 07:39:36 +0900

    행렬의 곱셈

--- src/Lv2/N12949.java
+++ src/Lv2/N12949.java
@@ -5,12 +5,12 @@ import java.util.Arrays;
 // 행렬의 곱셈
 public class N12949 {
     public static int[][] solution(int[][] arr1, int[][] arr2) {
-        int[][] result = new int[arr1.length][arr1[0].length];
+        int[][] result = new int[arr1.length][arr2[0].length];
         // result[i][j]는 arr1의 i행과 arr2의 j열의 곱의 합이다.
         for (int i = 0; i < result.length; i++) {
             for (int j = 0; j < result[i].length; j++) {
                 int sum = 0;
-                for (int k = 0; k < result[i].length; k++) {
+                for (int k = 0; k < arr1[0].length; k++) {
                     sum += arr1[i][k] * arr2[k][j];
                 }
                 result[i][j] = sum;
@@ -28,6 +28,18 @@ public class N12949 {
         System.out.println();
 
         arr = solution(new int[][]{{2, 3, 2}, {4, 2, 4}, {3, 1, 4}}, new int[][]{{5, 4, 3}, {2, 4, 1}, {3, 1, 1}});
+        for (int[] ints : arr) {
+            System.out.print(Arrays.toString(ints) + " ");
+        }
+        System.out.println();
+
+        arr = solution(new int[][]{{1, 2}, {2, 1}}, new int[][]{{1, 1, 1, 1}, {2, 2, 2, 2}});
+        for (int[] ints : arr) {
+            System.out.print(Arrays.toString(ints) + " ");
+        }
+        System.out.println();
+
+        arr = solution(new int[][]{{1, 2, 3}, {3, 2, 1}}, new int[][]{{1, 2}, {2, 1}, {1, 2}});
         for (int[] ints : arr) {
             System.out.print(Arrays.toString(ints) + " ");
         }
